31f7937dda40fb66dd2dad35ca153cf9e3057b51
asudemor <45600400+asudemor@users.noreply.github.com>
2022-01-01 01:12:00 +0300

    Add files via upload

---
+++ KodluyoruzUnity/arrayList/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace arrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList liste = new ArrayList();
+            //liste.Add("ayşe");
+            //liste.Add(2);
+            //liste.Add(true);
+            //liste.Add('A');
+
+            //içerisinde verilere erişim
+            //Console.WriteLine(liste[1]);
+
+            //foreach (var item in liste)
+                //Console.WriteLine(item);
+            
+            //addrange
+            //string[] renkler = {"kırmızı","sarı","yesil"};
+            List<int> sayilar = new List<int>(){1,8,3,7,9,92,5};
+            //liste.AddRange(renkler);
+            liste.AddRange(sayilar);
+
+            foreach (var item in liste)
+                Console.WriteLine(item);
+
+            //sort
+            liste.Sort();
+     
+            foreach (var item in liste)
+                Console.WriteLine(item);
+
+            //binary search
+            Console.WriteLine(liste.BinarySearch(9));
+
+            //reverse
+            liste.Reverse();
+
+            foreach (var item in liste)
+                Console.WriteLine(item);
+            
+            //clear
+            liste.Clear();
+
+            foreach (var item in liste)
+                Console.WriteLine(item);
+            
+            
+
+
+
+        }
+    }
+}
