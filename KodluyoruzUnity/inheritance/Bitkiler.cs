39c91ed2d7d83e84c5de46171057ee6ffe16ea92
asudemor <45600400+asudemor@users.noreply.github.com>
2022-01-02 19:36:30 +0300

    Add files via upload

---
+++ KodluyoruzUnity/inheritance/Bitkiler.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace inheritance
+{
+    public class Bitkiler:Canlilar
+    {
+        protected void Fotosentez()
+        {
+            Console.WriteLine("Bitkiler fotosentez yapar.");
+        }
+        public override void UyaranlaraTepki()
+        {
+            //base.UyaranlaraTepki(); // üst sınıftaki komutları çalıştırır.
+            Console.WriteLine("Bitkiler güneşe tepki verir.");
+        }
+    }
+    public class TohumluBitkiler:Bitkiler
+    {
+        public TohumluBitkiler()
+        {
+            base.Fotosentez();
+            base.Beslenme();
+            base.Bosaltim();
+            base.Solunum();
+            base.UyaranlaraTepki();
+        }
+        public void tohumlaCogalma()
+        {
+            Console.WriteLine("Tohumlu bitkiler tohumla çoğalır.");
+        }
+    } 
+    public class TohumsuzBitkiler:Bitkiler
+    {
+        public TohumsuzBitkiler()
+        {
+            base.Fotosentez();
+            base.Beslenme();
+            base.Bosaltim();
+            base.Solunum();
+        }
+
+        public void sporlaCogalma()
+        {
+            Console.WriteLine("Tohumsuz bitkiler sporla çoğalır.");
+        }
+    }
+}
---
+++ KodluyoruzUnity/inheritance/Hayvanlar.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace inheritance
+{
+    public class Hayvanlar:Canlilar
+    {
+        protected void Adaptasyon()
+        {
+            Console.WriteLine("Hayvanlar adaptasyon kurabilir.");
+        }
+        public override void UyaranlaraTepki()
+        {
+            base.UyaranlaraTepki(); // üst sınıftaki komutları çalıştırır.
+            Console.WriteLine("Hayvanlar temasa tepki verir.");
+        }
+    }
+    public class Surungenler:Hayvanlar
+    {
+        public Surungenler()
+        {
+            base.Adaptasyon();
+            base.Beslenme();
+            base.Bosaltim();
+            base.Solunum();
+        }
+        
+        public void surunerekHareketEtme()
+        {
+            Console.WriteLine("Sürüngenler sürünerek hareket eder.");
+        }
+    } 
+    public class Kuslar:Hayvanlar
+    {
+        public Kuslar()
+        {
+            base.Adaptasyon();
+            base.Beslenme();
+            base.Bosaltim();
+            base.Solunum();
+            base.UyaranlaraTepki();
+        }
+
+        public void ucmak()
+        {
+            Console.WriteLine("Kuşlar uçar.");
+        }
+    }
+
+}
