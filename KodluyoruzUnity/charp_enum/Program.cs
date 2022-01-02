29547c6a9164e426f540e49627762a07554aec34
asudemor <45600400+asudemor@users.noreply.github.com>
2022-01-02 17:25:10 +0300

    Add files via upload

---
+++ KodluyoruzUnity/charp_enum/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace charp_enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Gunler.Pazar);
+            Console.WriteLine((int)Gunler.Cumartesi);
+
+            int sicaklik = 32;
+            if (sicaklik <= (int)HavaDurumu.Normal)
+                Console.WriteLine("Dışarıya çıkmak için havanın ısınmasını bekle");    
+            else if (sicaklik >= (int)HavaDurumu.Sicak)
+                Console.WriteLine("Dışarıya çıkmak için çok sıcak bir gün.");    
+            else if (sicaklik >= (int)HavaDurumu.Normal && sicaklik <= (int)HavaDurumu.CokSicak)
+                Console.WriteLine("Dışarıya çıkmak için mğkemmel bir gün.");    
+            
+        }
+    }
+
+    enum Gunler
+    {
+        Pazartesi=1,
+        Sali,
+        Carsamba,
+        Persembe,
+        Cuma,
+        Cumartesi,
+        Pazar
+    }
+    enum HavaDurumu
+    {
+        Soguk = 5,
+        Normal = 20,
+        Sicak = 25,
+        CokSicak = 30
+    }
+}
