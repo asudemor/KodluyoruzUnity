7063fa8abad15b5fee2fd1cd70c011ce2cb22343
asudemor <45600400+asudemor@users.noreply.github.com>
2022-01-02 19:26:04 +0300

    Add files via upload

---
+++ KodluyoruzUnity/inheritance/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TohumluBitkiler tohumluBitki = new TohumluBitkiler();
+            tohumluBitki.tohumlaCogalma();
+
+            Kuslar marti = new Kuslar();
+            marti.ucmak();
+        }
+    }
+}
